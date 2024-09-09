077eb7f4891afc30b0cd47864b1eaee55136b8e6
dartrix <darwinramos2009@hotmail.com>
2024-09-09 01:40:11 -0400

    updates

--- init.lua
+++ init.lua
@@ -641,8 +641,8 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        -- But for many setups, the LSP (`tsserver`) will work just fine
+        -- tsserver = {},
         --
         emmet_ls = {
           capabilities = capabilities,
