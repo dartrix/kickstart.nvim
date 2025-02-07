528ed0354ed105ca6782aad38f56fc698a48e60f
Dartrix <darwinramos2009@hotmail.com>
2025-02-07 15:06:46 -0400

    js playground

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -69,4 +69,15 @@ return {
       }
     end,
   },
+  {
+    'mgastonportillo/js-playground.nvim',
+    dependencies = { 'rcarriga/nvim-notify' }, -- optional: custom notifications
+    cmd = 'JSPlayground',
+    init = function()
+      -- example mappings
+      vim.keymap.set('n', '<leader>jp', '<cmd>JSPlayground<CR>', { desc = 'Run JSPlayground' })
+      vim.keymap.set('n', '<leader>jx', '<cmd>JSPlayground stop<CR>', { desc = 'Stop JSPlayground' })
+    end,
+    opts = {}, -- or config = true
+  },
 }
